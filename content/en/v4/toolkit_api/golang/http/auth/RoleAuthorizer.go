f86ca8ad656ab20910148dc5051e4de268471da4
Eugenio Andrieu <68734409+EugenioCA@users.noreply.github.com>
2023-10-26 11:26:01 +0200

    Update RoleAuthorizer.go

--- content/en/v4/toolkit_api/golang/http/auth/RoleAuthorizer.go
+++ content/en/v4/toolkit_api/golang/http/auth/RoleAuthorizer.go
@@ -1,1 +1,39 @@
+---
+type: docs
+title: "RoleAuthorizer"
+linkTitle: "RoleAuthorizer"
+gitUrl: "https://github.com/pip-services4/pip-services4-go/tree/main/pip-services4-http-go"
+description: >
+    Provides methods to check on a user's roles.
+---
+
+### Description
+
+The RoleAuthorizer class provides methods to check a user's roles.
+
+### Instance methods
+
+#### userInRoles
+Checks the roles a user has been assigned.
+
+> `UserInRoles(roles []string) func(res http.ResponseWriter, req *http.Request, next http.HandlerFunc)
+
+- **roles**: string[] - roles list.
+- **returns**: (c *RoleAuthorizer)  - returns roles handler.
+
+#### userInRole
+Checks if the role is assigned to the user.  
+
+> ` UserInRole(role string) func(res http.ResponseWriter, req *http.Request, next http.HandlerFunc)
+
+- **role**: string - the user role.
+- **returns**: (c *RoleAuthorizer) - returns role handler.
+
+
+#### admin
+Checks if the user has admin role.  
+
+> `public` func(res http.ResponseWriter, req *http.Request, next http.HandlerFunc)
+
+- **returns**: Admin() - returns admin handler.
 
